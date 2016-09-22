7dfba6a0c1a06d52209dcfd0d1f7cc12cad871f3
raji11 <baranitharan117@gmail.com>
2016-09-22 10:53:13 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char s;
+printf("enter the character");
+scanf("%d",&s);
+if(s>a&&s<z) || (s>A&&s<Z)
+{
+printf("%c",is a alphabet);
+}
+else
+{
+printf("%c",is not a alphabet);
+}
+return 0;
+}
